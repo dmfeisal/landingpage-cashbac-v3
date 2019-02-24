2bd115d73d88346349b3d0bba47534b635ca0508
Jepri <jepri.sinaga@globalpay.id>
2019-02-24 23:55:41 +0700

    change source module name for qoutes

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Helmet from '../components/helmet';
 import Businessnavbar from '../components/businessnavbar';
-import Termscomponent from '../components/Termscomponent';
+import Termscomponent from '../components/termscomponent';
 import Footer from '../components/footer';
 
 const TermsPage = ({ children }) => (
