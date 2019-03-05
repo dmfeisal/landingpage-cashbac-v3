afb05f9616393ff25604ae680705b358f92f5cc2
Daeng Muhammad Feisal <daeng.feisal@globalpay.id>
2019-03-05 13:02:14 +0700

    setup hero banner text for homepage

--- src/components/hero.js
+++ src/components/hero.js
@@ -9,13 +9,13 @@ import { Link } from 'gatsby';
 const Hero = ({ siteTitle }) => (
 	<div>
 {/* MOBILE HERO*/}
-	<section className="section is-hidden-desktop is-hidden-tablet">
-		<div id="hero-homepage">
+	<section className="section is-hidden-desktop is-hidden-tablet" id="hero-homepage-mobile">
+		<div>
 			<div className="has-text-centered-mobile">
 	      		<h1 className="title hero-title-homepage has-text-centered-mobile">
 	      		Easiest way to get instant cashback. Anytime. Anywhere.
 	      		</h1>
-	      		<p className="subtitle hero-title-subtitle has-text-centered-mobile has-text-centered-mobile">
+	      		<p className="subtitle has-text-centered-mobile has-text-centered-mobile">
 	      		Cashbac is a free mobile app that gives you instant cashback for every transaction at your favorite merchants. With its features, Cashbac sets you free from many hassles of getting promos like topping up balances and allows you to get rewarded for eating, shopping, and taking care of yourself whenever and wherever you are.
 	      		</p>
 	      		
@@ -29,10 +29,10 @@ const Hero = ({ siteTitle }) => (
 		<div className="container center">
 			<div>
 	      		<div>
-	      		<h1 className="hero-title-homepage has-text-centered-desktop title">
+	      		<h1 className="hero-title-homepage has-text-centered-desktop has-text-centered-tablet title">
 	      		Easiest way to get instant cashback. Anytime. Anywhere.
 	      		</h1>
-	      		<p className="hero-subtitle-homepage subtitle has-text-centered-desktop">
+	      		<p className="hero-subtitle-homepage has-text-centered-desktop has-text-centered-tablet subtitle">
 	      		Cashbac is a free mobile app that gives you instant cashback for every transaction at your favorite merchants. With its features, Cashbac sets you free from many hassles of getting promos like topping up balances and allows you to get rewarded for eating, shopping, and taking care of yourself whenever and wherever you are.
 	      		</p>
 	      		</div>
