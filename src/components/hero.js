b356b989676e5dd4a09f581bb42375c7b88ccf39
Daeng Muhammad Feisal <daeng.feisal@globalpay.id>
2019-03-06 14:02:47 +0700

    update button download at mobile version

--- src/components/hero.js
+++ src/components/hero.js
@@ -20,6 +20,15 @@ const Hero = ({ siteTitle }) => (
 	      		<p className="subtitle has-text-centered-mobile has-text-centered-mobile">
 	      		Cashbac is a free mobile app that gives you instant cashback for every transaction at your favorite merchants, setting you free from many hassles in getting promos and rewarding yourself.
 	      		</p>
+	      		<div>
+					<a href="https://cashbac.onelink.me/1sXQ/homepage">
+					<img width="140px" src={buttonGooglePlay} alt="Get On Google Play" />
+					</a>
+					&nbsp; &nbsp;
+					<a href="https://cashbac.onelink.me/1sXQ/homepage">
+					<img width="140px" src={buttonAppStore} alt="Get On App Store" />
+					</a>
+				</div>	
 	      		
 	      	</div>
 	    </div>
