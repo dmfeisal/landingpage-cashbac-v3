aea2a654d76fffd0c7cedb4a510bddd7b8070a9a
Jepri Sinaga <jepri.sinaga@globalpay.id>
2019-04-02 14:11:06 +0700

    update information faq

--- src/components/faqcomponent.js
+++ src/components/faqcomponent.js
@@ -14,7 +14,7 @@ const Privacycomponent = () => (
 						<h2 className='margin--medium-b'>A guide to Cashbac</h2>
 						<hr className="spacer is-2"></hr>
 						<div>
-							<a className="faq" href="#How does Cashbac work?" rel="_self" title="How does Cashbac work?" ><div>How does Cashbac work?</div></a>
+							<a className="faq" href="#How-does-Cashbac-work?" rel="_self" title="How does Cashbac work?" ><div>How does Cashbac work?</div></a>
 							<div>
 								Cashbac is your personalized reward app. More than a discount, we offer you all the best deals in town. Discover all the best deals around you or find the best promos based on our recommendations. Simply link your credit card and tap your smartphone to our beacon technology or scan the QR code to make a purchase and receive instant cashback that you can use for your next purchase at all our participating merchants.
 							</div>
@@ -23,7 +23,7 @@ const Privacycomponent = () => (
 						<hr className="spacer is-2"></hr>
 
 						<div className='margin--medium-b'>
-							<a className="faq" href="#How do I earn instant Cashback?" rel="_self" title="How do I earn instant Cashback?" ><div>How do I earn instant Cashback?</div></a>
+							<a className="faq" href="#How-do-I-earn-instant-Cashback?" rel="_self" title="How do I earn instant Cashback?" ><div>How do I earn instant Cashback?</div></a>
 							<div>
 								When you pay with Cashbac app, you will automatically earn a percentage of your spending amount as a reward. The cashback amount will vary depending on the deals that you can see on Cashbac apps. Sometimes when the Merchants have special campaign, the deadlines do apply. You might not be able to enjoy the special campaign deal when the campaign is over. But do not worry, when this happens, you will still receive a normal Cashbac deal. These are stated in the Terms and Conditions.
 							</div>
@@ -32,7 +32,7 @@ const Privacycomponent = () => (
 						<hr className="spacer is-2"></hr>
 
 						<div className='margin--medium-b'>
-							<a className="faq" href="#Do I have to redeem cashback straight after I spend?" rel="_self" title="Do I have to redeem cashback straight after I spend?"><div>Do I have to redeem cashback straight after I spend?</div></a>
+							<a className="faq" href="#Do-I-have-to-redeem-cashback-straight-after-I-spend?" rel="_self" title="Do I have to redeem cashback straight after I spend?"><div>Do I have to redeem cashback straight after I spend?</div></a>
 							<div>
 								That is up to you. You can do it right away or wait to accumulate more and than redeem it later at any participating merchants.
 							</div>
@@ -50,7 +50,7 @@ const Privacycomponent = () => (
 						<hr className="spacer is-2"></hr>
 
 						<div className='margin--medium-b'>
-							<a className="faq" href="#Can I transfer my cashback to another person?" rel="_self" title="Can I transfer my cashback to another person?"><div>Can I transfer my cashback to another person?</div></a>
+							<a className="faq" href="#Can-I-transfer-my-cashback-to-another-person?" rel="_self" title="Can I transfer my cashback to another person?"><div>Can I transfer my cashback to another person?</div></a>
 							<div>
 								No, you cannot transfer you cashback balance to other Cashbac user. You can only use your cashback balance for your next purchase at all our participating merchants.
 							</div>
@@ -59,10 +59,15 @@ const Privacycomponent = () => (
 						<hr className="spacer is-2"></hr>
 
 						<div>
-							<a className="faq" href="#Can Cashbac deals combined with promos from merchant?" rel="_self" title="Can Cashbac deals combined with promos from merchant?"><div>Can Cashbac deals combined with promos from merchant?</div></a>
-						<div>
-							Some merchants allow to combine their promo with Cashbac and some do not. This is up to the merchant policy. To make sure, you may ask merhant person-in-charge for a combined promo with Cashbac.
+							<a className="faq" href="#Can-Cashbac-deals-combined-with-promos-from-merchant?" rel="_self" title="Can Cashbac deals combined with promos from merchant?"><div>Can Cashbac deals combined with promos from merchant?</div></a>
+							<div>
+								Some merchants allow to combine their promo with Cashbac and some do not. This is up to the merchant policy. To make sure, you may ask merhant person-in-charge for a combined promo with Cashbac.
+							</div>
 						</div>
+						<hr className="spacer is-2"></hr>
+						<div>
+							<a className="faq" href="#Can-Cashbac-deals-combined-with-promos-from-merchant?" rel="_self" title="Can Cashbac deals combined with promos from merchant?"><div>How does our 1 week rules promo?</div></a>
+							<div>Our 1 week count from sunday to tuesday, so don’t miss out and look for our promo</div>
 						</div>
 					</div>
 				</div>
