66fe0b72bbe4590a3d3059f7e653b8c865c7a931
Daeng Muhammad Feisal <daeng.feisal@globalpay.id>
2019-04-02 15:11:33 +0700

    edit 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,14 +9,14 @@ const NotFoundPage = () => (
 		<Helmet>
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
 			<meta name="description" content="Learn more about the terms and conditions of Cashbac application including how to register, link your card, make transactions and earn cashback rewards" />
-			<meta name="keywords" content="Cashbac privacy, Cashbac terms, Cashbac ketentuan" />
-			<title>404 page not found | Cashbac App</title>
+			<meta name="keywords" content="404 Page. Your page not found" />
+			<title>404 Page Not Found | Cashbac App</title>
 			<html lang="en" />
 			{/* Google / Search Engine Meta Tags */}
 			<meta itemprop="name" content="PT. Global Pay Indonesia" />
 			<meta
 				itemprop="description"
-				content="Learn more about the terms and conditions of Cashbac application including how to register, link your card, make transactions and earn cashback rewards"
+				content="404 Page Not Found"
 			/>
 			<meta itemprop="image" content="https://dl.dropboxusercontent.com/s/yp5xezri84yv0bn/logo-cashbac.png" />
 			<script type="text/javascript" src="https://unpkg.com/bulma-modal-fx/dist/js/modal-fx.min.js"></script>
